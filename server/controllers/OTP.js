a428b640a18edca84e89511a4453ff35840627dd
PatrickEinstein <mohammedola1234@gmail.com>
2023-05-17 06:51:08 -0400

    rec

--- server/controllers/OTP.js
+++ server/controllers/OTP.js
@@ -6,27 +6,31 @@ export const getOTP = async (req, res) => {
   try {
     const { matric } = req.body;
     const BIC = BICS.BIC;
-    const Voter = await BIC.find({ matric });
-    // console.log(Voter)
-    const email = Voter[0].email;
-    console.log(email);
-    const min = 1000;
-    const max = 9999;
-    const otp = Math.floor(Math.random() * (max - min + 1)) + min;
-    console.log(otp);
-    if (Voter) {
-      const newOTP = await new OTP({
-        otp,
-      });
-      const sentOTP = await newOTP.save();
-      mailer(email, "please Verify using the OTP code ", otp.toString());
-      return res.status(201).send({
-        success: true,
-        message: " Check your email for Verification link",
-        otp: otp,
-      });
+    if (!matric) {
+      res.status(400).send("please Provide your matric");
     } else {
-      return res.status(401).send("voter not found");
+      const Voter = await BIC.find({ matric });
+      // console.log(Voter)
+      const email = Voter[0].email;
+      console.log(email);
+      const min = 1000;
+      const max = 9999;
+      const otp = Math.floor(Math.random() * (max - min + 1)) + min;
+      console.log(otp);
+      if (Voter) {
+        const newOTP = await new OTP({
+          otp,
+        });
+        const sentOTP = await newOTP.save();
+        mailer(email, "please Verify using the OTP code ", otp.toString());
+        return res.status(201).send({
+          success: true,
+          message: " Check your email for Verification link",
+          otp: otp,
+        });
+      } else {
+        return res.status(401).send("voter not found");
+      }
     }
   } catch (err) {
     console.log(err);
