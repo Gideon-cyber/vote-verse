e8584fc300c1940d3316ea5d65ab2f3e18d34583
Gideon Nwokpor <gideonjohnson70@gmail.com>
2023-05-17 11:53:00 +0100

    update

--- client/components/login/AuthContainer.tsx
+++ client/components/login/AuthContainer.tsx
@@ -7,7 +7,10 @@ interface AuthContainerProps {
 const AuthContainer: React.FC<AuthContainerProps> = ({ children }) => {
   return (
     <div className="flex flex-col lg:flex-row-reverse items-center lg:justify-center w-full h-screen">
-      <div className="w-full lg:w-1/2 bg-auth-bg h-[20%] lg:h-full object-fill bg-no-repeat bg-center lg:bg-cover">
+      <div className="w-full lg:w-1/2 bg-auth-bg h-[20%] lg:h-full object-fill bg-no-repeat bg-center lg:bg-cover flex items-center justify-center">
+        <h1 className="text-white text-[30px] lg:text-[48px] leading-[150%] font-bold">
+          Vote-Verse
+        </h1>
         {/* <div className="hidden lg:block h-[200vh] w-[180px] bg-white blur-xl mr-10"></div> */}
       </div>
 
