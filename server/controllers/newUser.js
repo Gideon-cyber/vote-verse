e31dbf55112080f462d783620749da7a66d91410
PatrickEinstein <mohammedola1234@gmail.com>
2023-05-22 07:29:39 -0400

    voter is unique

--- server/controllers/newUser.js
+++ server/controllers/newUser.js
@@ -13,11 +13,16 @@ const CreateVoter = async (req, res) => {
     if (!firstName || !lastName || !matric || !email) {
       res.status(401).send("Supply all credentials");
     } else {
-      const createdVoter = await newVoter.save();
-      if (createdVoter) {
-        res.status(200).send(createdVoter);
+      const existingVoter = await BIC.find({ matric });
+      if (existingVoter) {
+        res.status(403).send("Voter has already been registered");
       } else {
-        res.status(401).send("failed to create User");
+        const createdVoter = await newVoter.save();
+        if (createdVoter) {
+          res.status(200).send(createdVoter);
+        } else {
+          res.status(401).send("failed to create User");
+        }
       }
     }
   } catch (err) {
