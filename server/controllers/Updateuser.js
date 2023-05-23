53ac3b049669c9af51061f0025557b63615e8ec2
PatrickEinstein <mohammedola1234@gmail.com>
2023-05-23 14:06:35 -0400

    null vote implemented

--- server/controllers/Updateuser.js
+++ server/controllers/Updateuser.js
@@ -27,20 +27,21 @@ export const UpdateThisUser = async (req, res) => {
       const filter = { matric: matric };
       const update = {
         $set: {
-          president: candidate1,
-          vicePresident: candidate2,
-          sport: candidate3,
-          gensec: candidate4,
-          social: candidate5,
-          pro: candidate6,
-          finsec: candidate7,
-          treasurer: candidate8,
-          AGS: candidate9,
+          president: candidate1 ? candidate1 : null,
+          vicePresident: candidate2 ? candidate2 : null,
+          sport: candidate3 ? candidate3 : null,
+          gensec: candidate4 ? candidate4 : null,
+          social: candidate5 ? candidate5 : null,
+          pro: candidate6 ? candidate6 : null,
+          finsec: candidate7 ? candidate7 : null,
+          treasurer: candidate8 ? candidate8 : null,
+          AGS: candidate9 ? candidate9 : null,
           Voted: true,
         },
       };
       const options = { new: true };
       const updatedUser = await BIC.findOneAndUpdate(filter, update, options);
+      res.send({ message: "You have successfully voted" });
       console.log(updatedUser);
       if (updatedUser) {
         res.status(200).send(updatedUser);
